f84f797d37527302c05ddee78b41246ce8bbada2
nasir2712 <nasir.mohd2712@gmail.com>
2017-12-29 12:33:47 -0800

    reverting some changes

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -95,7 +95,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
@@ -110,7 +110,7 @@ module.exports = function (config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
